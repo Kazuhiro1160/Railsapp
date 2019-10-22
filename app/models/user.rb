5d09c292ad4e47244177a0b7b8e1b5e298e3e046
Kazuhiro1160 <mkazu0925>
2019-10-22 17:45:38 +0000

    Use SSL and the Puma webserver in production

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,14 +5,8 @@ class User < ApplicationRecord
   validates :email,   presence: true,  length: { maximum: 255 },
                       format: { with: VALID_EMAIL_REGEX},
                       uniqueness: { case_sensitive: false }
-<<<<<<< HEAD
-  
-  has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
-=======
   validates :grade, presence: true 
   
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
->>>>>>> sign-up
 end
